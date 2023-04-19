ea2d9a5b0677b084bfd7d918278ac3875dcb0671
Gorgud N. Nizayli <gorgudnn@code.local>
2023-04-19 11:45:06 +0400

    login method with while

---
+++ Login(Loop)/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string kullaniciAdi = "admin";
+        string sifre = "123";
+
+        while (true)
+        {
+            Console.WriteLine("\nKullanici adi: ");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Sifre: ");
+            string password = Console.ReadLine();
+
+            if (username == kullaniciAdi && password == sifre)
+            {
+                Console.WriteLine("Giris edildi!");
+                return;
+                //break;
+            }
+            else Console.WriteLine("Yanlis bilgi!");
+        }
+    }
+}
